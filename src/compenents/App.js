2895d30ea4b47ac7e8ba001e653e7b68e7460757
Rahul Kaliambi Raghavan <kaliambiraghavanr@cardiff.ac.uk>
2022-05-24 10:52:02 +0100

    modified deal items to add animation

--- src/compenents/App.js
+++ src/compenents/App.js
@@ -1,75 +1,110 @@
-import React from 'react'
+import React from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
-
-import ajax from './Ajax';
+import {
+  View,
+  Text,
+  Animated,
+  Easing,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
+import ajax from '../ajax';
 import DealList from './DealList';
 import DealDetail from './DealDetail';
-import SearchBar from './Searchbar';
+import SearchBar from './SearchBar';
 
-class App extends React.Component{
-
-    state = {
-        deals : [],
-        currentDealId: null
-    }
-
-    async componentDidMount(){
+class App extends React.Component {
+  titleXPos = new Animated.Value(0);
+  state = {
+    deals: [],
+    dealsFromSearch: [],
+    currentDealId: null,
+    activeSearchTerm: '',
+  };
+  animateTitle = (direction = 1) => {
+    const width = Dimensions.get('window').width - 150;
+    Animated.timing(this.titleXPos, {
+      toValue: direction * (width / 2),
+      duration: 1000,
+      easing: Easing.ease,
+    }).start(({ finished }) => {
+      if (finished) {
+        this.animateTitle(-1 * direction);
+      }
+    });
+  };
+  async componentDidMount() {
+    this.animateTitle();
     const deals = await ajax.fetchInitialDeals();
-    this.setState((prevState)=>{
-        return {deals};
-    })
+    this.setState({ deals });
+  }
+  searchDeals = async (searchTerm) => {
+    let dealsFromSearch = [];
+    if (searchTerm) {
+      dealsFromSearch = await ajax.fetchDealSearchResults(searchTerm);
     }
+    this.setState({ dealsFromSearch, activeSearchTerm: searchTerm });
+  };
+  setCurrentDeal = (dealId) => {
+    this.setState({
+      currentDealId: dealId,
+    });
+  };
+  unsetCurrentDeal = () => {
+    this.setState({
+      currentDealId: null,
+    });
+  };
+  currentDeal = () => {
+    return this.state.deals.find((deal) => deal.key === this.state.currentDealId);
+  };
+  render() {
+    if (this.state.currentDealId) {
+      return (
+        <View style={styles.main}>
+          <DealDetail
+            initialDealData={this.currentDeal()}
+            onBack={this.unsetCurrentDeal}
+          />
+        </View>
+      );
+    }
+    const dealsToDisplay =
+      this.state.dealsFromSearch.length > 0
+        ? this.state.dealsFromSearch
+        : this.state.deals;
 
-    setCurrentDeal = (dealId) =>{
-        this.setState({
-            currentDealId : dealId
-        });
+    if (dealsToDisplay.length > 0) {
+      return (
+        <View style={styles.main}>
+          <SearchBar
+            searchDeals={this.searchDeals}
+            initialSearchTerm={this.state.activeSearchTerm}
+          />
+          <DealList deals={dealsToDisplay} onItemPress={this.setCurrentDeal} />
+        </View>
+      );
     }
-
-    currentDeal = () => {
-        return this.state.deals.find(
-            (deal) => deal.key === this.state.currentDealId
-        )
-    }
-
-    render(){
-        if(this.state.currentDealId){
-            return (
-                <View style={styles.main}>
-                <DealDetail initialDealData={this.currentDeal()}/>
-                </View>
-            )
-        }
-        if(this.state.deals.length > 0){
-           return(
-               <View style={styles.main}>
-               <SearchBar/>
-            <DealList deals={this.state.deals} onItemPress={this.setCurrentDeal}/>
-            </View>)
-        }
-        return(
-            <View style={styles.container}>
-                {
-                  <Text style={styles.header}>Bakesale</Text>
-                }
-            </View>
-        );
-    }
+    return (
+      <Animated.View style={[{ left: this.titleXPos }, styles.container]}>
+        <Text style={styles.header}>Bakesale</Text>
+      </Animated.View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex :1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    header:{
-        fontSize: 40
-    },
-    main:{
-        marginTop:30
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main: {
+    marginTop: 30,
+  },
+  header: {
+    fontSize: 40,
+  },
+});
 
 export default App;
